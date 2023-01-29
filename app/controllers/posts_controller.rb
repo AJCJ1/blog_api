39024e2aa797aec48f9cc61c4d9837f92d248313
arnold cubici-jones <arnoldcubicijones@hotmail.co.uk>
2023-01-29 15:34:18 +0000

    Creates routes, controllers and Post model with seeds

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,17 @@
+class PostsController < ApplicationController
+  ActiveModelSerializers.config.default_includes = '**'
+  ActiveModelSerializers.config.adapter = :json
+  def index
+    @posts = Post.all
+    render json: @posts, include: [:image]
+  end
+
+  def details
+    @post = Post.find_by(:id => params[:id])
+    if @post.nil?
+      redirect_to posts_path
+    else
+      render json: @post, include: [:image]
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,138 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Post.create(title: 'Perfect Pole Work',
+            description: 'A Fun Exercise for You and Your Horse',
+            content:
+            "
+            Cavaletti poles are a useful training tool due to their role in making joints more supple and improving the individual’s stamina and muscle tone. Advantages of cavaletti work include minimising the incidence of injuries, improving performance for competitions, delaying the onset of fatigue, maintaining willingness, and improving skills and work capacity. Poles should be set up with a rough distance of 3-3.5 feet between them for walk poles and then 4-4.5 feet apart for trot poles to ensure the distance between the poles fits the individuals stride length. The horse should initially be walked, in hand, over the walk poles and this should be done both ways. Poles can be adjusted from this so they are further apart, as trot poles, and the horse can be trotted in hand over the poles; again, making sure to do this in both directions. Cavaletti poles should gradually be increased in difficulty, but they shouldn’t be overused as this can lead to injury. They can be made more challenging by raising them, starting with raising one end at a time and initially progressing to both ends raised. This can also be done at walk and trot and then with a rider on top.
+
+              [img:https://imagestoragemvp.s3.eu-west-2.amazonaws.com/horse-poles.webp]
+
+            Pole work enables the horse to use its limbs more and increase the demand through the legs without compromising the horse’s gait. The horse is encouraged to move rhythmically and to swing through their backs. Muscles can be built up using poles as the horse is asked to work in a controlled way and they are encouraged to flex their limbs to lift their feet above the poles which requires core engagement. Poles aid recovery as they release muscle tension. This works as they encourage a low neck that is stretched forwards and down which causes back muscles, such as the longissimus dorsi, to contract and relax thus reducing tightness.
+
+
+            A study by Shaw et al., 2021, analysed the effect of ground poles on the longissimus dorsi and rectus abdominus muscle activity during walk and trot. This study showed that walking over poles drastically increased bilateral muscle activity in these two muscles. It also highlighted how trotting over poles mainly increased activity within the rectus abdominus muscle. By improving performance and biomechanical efficiency of movement in horses, posture control and core engagement are involved. These improvements reduce the load on the limbs which reduces the chance of injuries.
+
+
+            Posture control and spinal positioning are influenced by many muscle groups including hypaxial and epaxial muscles, hindquarter muscles and abdominal core muscles. Pole work involving raised poles has been proven to increase stride length, tracking distance and increase the cross-sectional area of the multifidi muscle. During the swing phase, the individual must be balanced which requires the body and limbs to maintain stability. The study concluded that the use of ground poles within a rehabilitation programme is expected to increase core abdominal activation and recruitment of epaxial muscles.
+
+              [img:https://imagestoragemvp.s3.eu-west-2.amazonaws.com/blog1.webp]
+
+            Another study, by Brown et al., 2014, evaluated the swing phase kinematics of horses when trotting over poles and explained how Cavaletti poles can be used therapeutically to restore full range of motion in limb joints. Pole work has been shown to increase joint flexion which activates and strengthens flexor musculature which also requires neuromotor coordination and proprioceptive awareness to clear the poles. Pole work also requires flexion of the hip joints which determines the range of hindlimb protraction and retraction during both gaits. The study determined that concentric muscular contractions of the fore and hindlimbs are required during the swing phase when a horse trots over poles which involves the elbow, hip, and tarsal joints which highlights the benefit and overall effect this technique has on the horse.
+
+
+            Horses can also be ridden over Cavaletti poles as you can see in these videos which encourages them to work more correctly and engage their hindlimbs more. The addition of a rider to pole work increases the load for the horse and challenges them which also improves neuromotor control and function alongside the physical benefits mentioned.
+
+              [video:https://imagestoragemvp.s3.eu-west-2.amazonaws.com/blog1vid.mp4]
+
+            ",
+            references: "Brown, S., Stubbs, N., Kaiser, L., Lavagnino, M and Clayton, H. 2014.’Swing Phase Kinematics of Horses Trotting Over Poles’, Equine Veterinary Journal, 47(1), pp. 107-112.
+            Klimke, I and Klimke, R. (2018) Cavalletti for Dressage and Jumping. Marlborough: The Crowood Press.
+            Paulekas, R and Haussler. 2009. ‘Principles and Practice of Therapeutic Exercise for Horses’, Journal of Equine Veterinary Science, 29(12), pp. 870-893.
+            Riber, C., Castejon-Riber, C., Rubio, M., Agüera, E and Muñoz, A. 2017. ‘Objectives, Principles, and Methods of Strength Training for Horses’, Journal of Equine Veterinary Science, 56(1), pp. 93-103.
+            Shaw, K., Ursini, T., Levine, D., Richards, J and Adair, S. 2021. ‘The Effect of Ground Poles and Elastic Resistance Bands on Longissimus Dorsi and Rectus Abdominus Muscle Activity During Equine Walk and Trot’, Journal of Equine Veterinary Science, 107(1), pp. 103772."
+)
+Post.create(title: 'The Benefits of Myofascial Release',
+            description: "An Owner's Easy Guide",
+            content:
+            "
+            Myofascial release is a soft tissue manual therapy that involves the application of pressure to muscles and connective tissue. The elastic properties of muscles within horses means great effects of mobilisation of muscle through myofascial release techniques can be achieved.
+
+
+            Myofascial release can benefit issues such as:
+
+            · Trigger points (tight muscle points within tight muscle fibres)
+
+            · Muscle tension and imbalances
+
+            · Pain relief
+
+            · Muscle stiffness
+
+            · Muscle restrictions causing reduced movement
+
+            · Muscle spasm
+
+
+            Improving these issues can result in:
+
+            · Improved bascule over fences
+
+            · Increased stride length
+
+            · Improved cross-country time
+
+            · Reduced recovery time between competitions
+
+            · Reduced chance of injury
+
+            · Increased power and flexibility – often leading to better scores
+
+            Myofascial release involves the application of a long load and long duration stretch to the affected muscular regions. A light pressure is applied to the issue, such as a trigger point, for several seconds until a release of the tissue can be felt.
+
+              [img:https://imagestoragemvp.s3.eu-west-2.amazonaws.com/foal.webp]
+
+            Myofascial release is conducted following these steps:
+
+            1. Ensure the horse is comfortable and relaxed. Ideally this should take place in a stable 		   where the horse is secure and unable to escape.
+
+            2. Locate the tight muscle fibres (no spring should be felt when touching the muscle and the muscle may look tighter than others during movements).
+
+            3. Apply gentle pressure with fingertips to the area by pushing down into the muscle. 		   Ensure to watch the horse for signs of discomfort (ears going back, moving away, lifting a leg).
+
+            4.Pressure should continue to be applied until the “springy feel” stops and a firm barrier is felt.
+
+            5. Pressure can continue to be held when this hard feeling muscle barrier is felt.
+
+            6. Pressure can be stopped when a ‘release’ is seen – this includes licking, chewing, or yawning or when this hard barrier seems to soften.
+
+              [img:https://imagestoragemvp.s3.eu-west-2.amazonaws.com/human-physio.webp]
+
+            It works in the same way that a massage for humans works - and it relaxes your pets too!
+
+            How does this work and when can it be used?
+
+            Myofascial release works because of the viscoelasticity of the fascial tissue (the elasticity within the tissue allowing it to stretch without being damaged).
+
+            The best times for myofascial release are before a competition to warm the muscles up and after the competition to relax the muscles and remove any toxins after the horse has worked.
+
+            This technique can also be done when being worked in the horse’s weekly routine but no more than 1-2 times per week.
+
+            If you suspect your horse requires this more frequently or is showing signs of discomfort, contact a Vet or Vet physio.
+
+            Any muscle can be targeted with myofascial release and a this is a tool that can be administered anywhere and at any time – perfect for when travelling for competitions!
+
+            Areas to target:
+
+            Many studies have been done which highlight the benefits of myofascial release within competition horses. One study conducted in 2015 concluded that myofascial release therapy “shows encouraging effectiveness, emerging as a strategy with a solid evidence base and tremendous potential”. Myofascial release can also result in an increased blood flow to the area targeted which can reduce any swelling or build-up of fluids. Horses, particularly those in work, are often subjected to areas of stress and strain in their bodies. Below are the common areas of equine stress points which can be targeted with this technique:
+
+            What causes the issues targetted?
+
+            Muscle tension has been known to result from repeated contractions which will be common in competing horses due to the demand required on their bodies.
+
+            Some muscles perform more work than others which can result in imbalances. Some of these muscles often become overstressed than others which results in tension, such as trigger points (tight points within tight muscle fibres).
+
+            Addressing issues within the myofascial system will result in optimal performance in the competition horse. An example of where myofascial release is beneficial is shown below: trigger points beneath the shoulder can often restrict movement within the forelimb and myofascial release will reduce these.
+
+              [img:https://imagestoragemvp.s3.eu-west-2.amazonaws.com/black-horse-physio-working.webp]
+
+            Data confirms the benefit and necessity of myofascial release as a therapy tool due to a study, also in 2015, that assessed the effects of myofascial release in 6 horses. Results highlighted a 58% decrease in muscle tension across multiple muscles more than 48 hours after treatment which shows the length of muscle relaxation. From this, it is evident that myofascial release is essential in allowing the horse to relax and reduce stress/tension that has built up during their work. Adopting this technique can significantly reduce the chance of injury to overused muscles and allow the horse to recover more quickly so they are ready for the next competition. The easy addition of myofascial release to your routine may be the tool you need to win.
+
+            [img:https://imagestoragemvp.s3.eu-west-2.amazonaws.com/horse-human-kiss.webp]
+
+            For more information on Myofascial Release and Manual Therapies take a look at our treatment section on our website.
+
+            ",
+            references: "1.     Acutt, E., Jeune, S., and Pypendop, B. 2019. ‘Evaluation of the Effects of Chiropractic on Static and Dynamic Muscle Variable in Sport Horses’, Journal of Equine Veterinary Science, 72(1), pp. 84-90.
+
+            2.     Brockman, T. 2017. ‘A Case Study Utilising Myofascial Release, Acupressure and Trigger Point Therapy to Treat Bilateral “Stringhalt” in a 12-Year-old Akhal-Teke Horse’, Journal of Bodywork and Movement Therapies, 21(3), pp. 589-593.
+
+            3.     Davis, C. (2009) Complementary Therapies in Rehabilitation: Evidence for Efficacy in Therapy. New Jersey: SLACK Incorporated.
+
+            4.     Goff, L. 2009. ‘Manual Therapy for the Horse – A Contemporary Perspective’, Journal of Equine Veterinary Science, 29(11), pp. 799-808.
+
+            5.     Harrison, A., Elbrønd, V and Krasnodebska, M. 2015. ‘Multi-Frequency Bioimpedance and Myofascial Release Therapy: An Equine “Atlasorange1” Validation Study’, Medical Research Archives, 3(3), pp.1-14.
+
+            6.     Scott, M and Swenson, L. 2009. ‘Evaluating the Benefits of Equine Massage Therapy: A Review of the Evidence and Current Practices’, Journal of Equine Veterinary Science, 29(9), pp. 687-697."
+)
+puts "Your DB has been seeded"
+# to add:
+# videos
